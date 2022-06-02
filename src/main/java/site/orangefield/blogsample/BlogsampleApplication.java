a45a465ce6f61e165df0f68c2f8d8fd266db62b1
field <jsw_777@naver.com>
2022-06-02 15:21:49 +0900

    Initial Project

---
+++ src/main/java/site/orangefield/blogsample/BlogsampleApplication.java
@@ -0,0 +1,13 @@
+package site.orangefield.blogsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/orangefield/blogsample/BlogsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package site.orangefield.blogsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
